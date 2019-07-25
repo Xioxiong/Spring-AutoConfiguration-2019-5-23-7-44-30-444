0f77c802ea3277bf8bb96630f7e9560feebca783
xiongwei <2894001817@qq.com>
2019-07-25 19:50:35 +0800

    配置了properties文件，并测试通过

--- src/main/java/com/zhengdianfang/demo/User.java
+++ src/main/java/com/zhengdianfang/demo/User.java
@@ -1,14 +1,18 @@
 package com.zhengdianfang.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class User {
 
+    @Value("${com.zhengdianfang.demo.user.user}")
     private String username;
 
+    @Value("${com.zhengdianfang.demo.user.age}")
     private int age;
 
+    @Value("${com.zhengdianfang.demo.user.address}")
     private String address;
 
     public String getUsername() {
